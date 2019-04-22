b6db028f9606cebf4c4ccbc5eb26467688c40dba
vadxq <dongxianlin@vadxq.com>
2019-04-22 20:26:14 +0800

    add build dis

---
+++ server/cqhttp/html/dist/precache-manifest.4f0908ac2f27cb1821e8b0dee3046bca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c73d1987a08a49ad09ed",
+    "url": "/js/chunk-vendors.a6426063.js"
+  },
+  {
+    "revision": "4e30deb0f58352d7fe49",
+    "url": "/js/app.850d4d56.js"
+  },
+  {
+    "revision": "e3265da0b912786c641c",
+    "url": "/js/about.69260e02.js"
+  },
+  {
+    "revision": "b681510065198d8bbb92eb3faf800e65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e30deb0f58352d7fe49",
+    "url": "/css/app.519fc128.css"
+  }
+];
